78f16716b48387e10c542aad89be1316d13bda21
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-10-10 11:03:53 -0500

    scaling numeric data

---
+++ regression/split_scale.py
@@ -0,0 +1,39 @@
+# Scaling Numeric Data exercise
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+
+import wrangle
+import env
+
+
+df = wrangle.wrangle_telco()
+
+# 1.) split_my_data(X, y, train_pct)
+
+
+
+# 2.) standard_scaler()
+
+
+
+# 3.) scale_inverse()
+
+
+
+# 4.) uniform_scaler()
+
+
+
+# 5.) gaussian_scaler()
+
+
+
+# 6.) min_max_scaler()
+
+
+
+# 7.) iqr_robust_scaler()

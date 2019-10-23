013f1e0122b56414429b2f3192495e8cd7d661c2
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-10-23 15:02:20 -0500

    prepare functions

--- classification/prepare.py
+++ classification/prepare.py
@@ -26,3 +26,32 @@ def prep_titanic(titanic):
     scaler = MinMaxScaler()
     titanic_train[['fare', 'age']] = scaler.fit_transform(titanic_train[['fare', 'age']])
     return titanic_train, titanic_test, scaler
+
+# Alternative way to prep titanic df
+
+def titanic_missing_fill(titanic):
+    titanic.embark_town.fillna('Other', inplace = True)
+    titanic.embarked.fillna('Unknown', inplace = True)
+    return titanic
+
+def titanic_remove_columns(titanic):
+    return titanic.drop(columns = ['deck'])
+
+def encode_titanic(titanic):
+    encoder = LabelEncoder()
+    encoder.fit(titanic.embarked)
+    titanic.embarked = encoder.transform(titanic.embarked)
+    return titanic, encoder
+
+def scale_titanic(titanic):
+    scaler = MinMaxScaler()
+    scaler.fit(titanic[['age','fare']])
+    titanic[['age','fare']] = scaler.transform(titanic[['age','fare']])
+    return titanic, scaler
+
+def prep_titanic(titanic):
+    titanic = titanic_missing_fill(titanic)
+    titanic = titanic_remove_columns(titanic)
+    titanic, encoder = encode_titanic(titanic)
+    titanic, scaler = scale_titanic(titanic)
+    return titanic, encoder, scaler

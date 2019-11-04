a82daffbce634eb313b6d71c4f4317db5a407a48
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-11-04 17:08:11 -0600

    commit changes

--- clustering/wrangle_zillow.py
+++ clustering/wrangle_zillow.py
@@ -1,15 +1,14 @@
-#Functions of the work above needed to aquire and prepare
-#a new sample of data
-
 import acquire
 import prepare
 
 def wrangle_zillow_data():
-    df = acquire_zillow()
-    df = zillow_single_unit(df)
-    df = remove_columns(df,['calculatedbathnbr','finishedsquarefeet12',\
+    df = acquire.acquire_zillow()
+    df = prepare.zillow_single_unit(df)
+    df = prepare.remove_columns(df,['calculatedbathnbr','finishedsquarefeet12',\
         'fullbathcnt','propertycountylandusecode','unitcnt','structuretaxvaluedollarcnt',\
         'landtaxvaluedollarcnt','assessmentyear','propertyzoningdesc'])
-    df = handle_missing_values(df)
+    df = prepare.handle_missing_values(df)
     df.dropna(inplace = True)
     return df
+
+ #df = wrangle_zillow_data()   

5650964767450931704c9c1a38ea8719ac606a71
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-11-04 16:12:11 -0600

    commit change

---
+++ clustering/wrangle_zillow.py
@@ -0,0 +1,15 @@
+#Functions of the work above needed to aquire and prepare
+#a new sample of data
+
+import acquire
+import prepare
+
+def wrangle_zillow_data():
+    df = get_zillow_data()
+    df = zillow_single_unit(df)
+    df = remove_columns(df,['calculatedbathnbr','finishedsquarefeet12',\
+        'fullbathcnt','propertycountylandusecode','unitcnt','structuretaxvaluedollarcnt',\
+        'landtaxvaluedollarcnt','assessmentyear','propertyzoningdesc'])
+    df = handle_missing_values(df)
+    df.dropna(inplace = True)
+    return df

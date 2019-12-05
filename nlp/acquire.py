170cdc0ba7f5718720084c3f6b043ac43276f02e
Tim Sotirhos <tim.sotirhos@gmail.com>
2019-12-05 15:58:43 -0600

    completed acquire

--- nlp/acquire.py
+++ nlp/acquire.py
@@ -31,7 +31,7 @@ def get_blog_articles():
     if path.exists('Codeup_blog_articles.json'):
         with open('Codeup_blog_articles.json') as f:
             return json.load(f)
-            
+
     urls = [
         "https://codeup.com/codeups-data-science-career-accelerator-is-here/",
         "https://codeup.com/data-science-myths/",
@@ -52,5 +52,113 @@ def get_blog_articles():
 
     return output
 
-articles = get_blog_articles()
-articles
+
+def get_news_articles():
+    filename = 'inshorts_news_articles.csv'
+
+    # check for presence of the file or make a new request
+    if os.path.exists(filename):
+        return pd.read_csv(filename)
+    else:
+        return make_new_request()
+
+def get_articles_from_topic(url):
+    headers = {'user-agent': 'Codeup Bayes Instructor Example'}
+    response = get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    output = []
+
+    articles = soup.select(".news-card")
+
+    for article in articles: 
+        title = article.select("[itemprop='headline']")[0].get_text()
+        content = article.select("[itemprop='articleBody']")[0].get_text()
+        author = article.select(".author")[0].get_text()
+        published_date = article.select(".time")[0]["content"]
+        category = response.url.split("/")[-1]
+
+        article_data = {
+            'title': title,
+            'content': content,
+            'category': category,
+            'author': author,
+            'published_date': published_date,
+        }
+        output.append(article_data)
+
+
+    return output
+
+
+def make_new_request():
+    urls = [
+        "https://inshorts.com/en/read/business",
+        "https://inshorts.com/en/read/sports",
+        "https://inshorts.com/en/read/technology",
+        "https://inshorts.com/en/read/entertainment"
+    ]
+
+    output = []
+    
+    for url in urls:
+        # We use .extend in order to make a flat output list.
+        output.extend(get_articles_from_topic(url))
+
+    print("stuff")
+    print(output)
+    df = pd.DataFrame(output)
+    df.to_csv('inshorts_news_articles.csv') 
+
+    return df
+
+# Alternative solution for acquire Codeup_blog
+'''
+def get_blog_posts():
+    filename = './codeup_blog_posts.csv'
+
+    # check for presence of the file or make a new request
+    if os.path.exists(filename):
+        return pd.read_csv(filename)
+    else:
+        return make_new_request()
+
+def make_dictionary_from_article(url): 
+    # Set header and user agent to increase likelihood that your request get the response you want
+    headers = {'user-agent': 'Codeup Bayes Instructor Example'}
+
+    # This is the actual HTTP GET request that python will send across the internet
+    response = get(url, headers=headers)
+
+    # response.text is a single string of all the html from that page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.title.get_text()
+    body = soup.select("div.mk-single-content.clearfix")[0].get_text()
+
+    output = {}
+    output["title"] = title
+    output["body"] = body
+    return output
+
+
+def make_new_request():
+    urls = [
+        "https://codeup.com/codeups-data-science-career-accelerator-is-here/",
+        "https://codeup.com/data-science-myths/",
+        "https://codeup.com/data-science-vs-data-analytics-whats-the-difference/",
+        "https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/",
+        "https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/",
+    ]
+
+    output = []
+    
+    for url in urls:
+        article_dictionary = make_dictionary_from_article(url)
+        output.append(article_dictionary)
+
+    df = pd.DataFrame(output)
+    df.to_csv('./codeup_blog_posts.csv') 
+
+    return df
+'''
